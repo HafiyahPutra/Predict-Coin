f8cb2a7f7b11f40073f55cdbcc6dc4327263277c
Hafiyah Putra <134901255+HafiyahPutra@users.noreply.github.com>
2025-05-05 22:35:02 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,26 +1,9 @@
-# train_model.py
-import requests
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-import joblib
+# app/main.py
 
-url = "https://api.coingecko.com/api/v3/coins/bitcoin/ohlc?vs_currency=usd&days=1"
-headers = {
-    "x-cg-demo-api-key": "CG-7pi9DCcf6E6PmCFBLrwvGtZT"
-}
+from fastapi import FastAPI
 
-res = requests.get(url, headers=headers)
-data = res.json()
+app = FastAPI()
 
-df = pd.DataFrame(data, columns=["timestamp", "open", "high", "low", "close"])
-df["timestamp"] = pd.to_datetime(df["timestamp"], unit="ms")
-
-# Fitur dan target
-X = df[["open", "high", "low"]]
-y = df["close"]
-
-model = LinearRegression()
-model.fit(X, y)
-
-joblib.dump(model, "model/price_model.pkl")
-print("Model saved!")
+@app.get("/")
+def read_root():
+    return {"message": "Hello, Predict Coin is live!"}
